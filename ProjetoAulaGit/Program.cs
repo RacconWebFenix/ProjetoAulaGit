acb01e17ee75783a35553d6fa529ff2c5c2c72f1
Marcos Domingues <dfenixweb@gmail.com>
2021-02-15 09:46:38 -0300

    Acrescentada mentagem de BOM DIA

--- ProjetoAulaGit/Program.cs
+++ ProjetoAulaGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoAulaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
